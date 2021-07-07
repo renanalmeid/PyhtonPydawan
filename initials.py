8a01fdc397603ebcaf4bbd1a37815c53b40072b9
renanalmeid <souza.almeida.renan@hotmail.com>
2021-07-07 01:33:35 -0300

    First
    RA

---
+++ initials.py
@@ -0,0 +1,17 @@
+#my first dumb code 
+
+#Renan Almeida
+
+#Fun fact: love learning languages
+
+name1 = """
+ RRRR     A
+ R   R   A A
+ R   R  A   A
+ RRRR   AAAAA
+ R R    A   A
+ R  R   A   A
+ R   R  A   A
+"""
+
+print(name1)
